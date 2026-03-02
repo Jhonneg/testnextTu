0ad020d59fee0e2d7c7db58d58a643e032148f21
Jhonne <jonee4339@hotmail.com>
2026-03-02 08:25:20 -0300

    added logo and favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,19 +3,18 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { Menu, X, Phone } from "lucide-react";
+import Image from "next/image";
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-md border-b border-gray-100 shadow-sm transition-all">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white backdrop-blur-md border-b border-gray-100 shadow-sm transition-all">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="shrink-0 cursor-pointer">
             <Link href="/" className="flex items-center gap-2">
-              <div className="w-10 h-10 bg-blue-600 rounded-lg flex items-center justify-center text-white font-bold text-xl">
-                T
-              </div>
+              <Image src="/tupilogo.png" height={120} width={120} />
             </Link>
           </div>
 
